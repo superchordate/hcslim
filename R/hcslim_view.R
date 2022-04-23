1f276bf8e88e7556c1e906ed223a545e3d0c13d4
bryce <superchordate@gmail.com>
2022-04-23 16:40:56 -0500

    Add printtempfile option

--- R/hcslim_view.R
+++ R/hcslim_view.R
@@ -7,6 +7,7 @@
 #' @param class "chart" in Highcharts.chart(... For Highmaps, it should be "mapChart".
 #' @param printjs Print completed JS to console for troubleshooting or pasting into jsFiddle.
 #' @param pretty Use pretty formatting when converting options to JSON.
+#' @printtempfile Print the location of the temporary HTML file. Useful for troubleshooting.
 #'
 #' @return Creates a temp HTML file and shows it in the viewer. 
 #' @export
@@ -44,11 +45,13 @@ hcslim_view = function(
     usecode_include = 'highcharts',
     class = c('chart', 'mapChart', 'stockChart', 'ganttChart'),
     printjs = FALSE, 
-    pretty = !printjs
+    pretty = !printjs,
+    printtempfile = FALSE
 ){
   
   # get a temp file and use it to build a div id.
   tempfile = tempfile(fileext = ".html")
+  if(printtempfile) print(tempfile)
   id = gsub('[.]html', '', gsub('.+\\\\', '', tempfile))
   
   # build the chart from options, and the module import.
