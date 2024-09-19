d967b6d66a60c197c56f802f36f8982b0f0ead25
Bryce Chamberlain <superchordate@gmail.com>
2024-09-19 11:03:29 -0600

    Fix JS imports for widget.

--- inst/htmlwidgets/lib/highcharts-defaults.js
+++ inst/htmlwidgets/lib/highcharts-defaults.js
@@ -1,43 +1,43 @@
-document.addEventListener('DOMContentLoaded', function() { Highcharts.setOptions({
-    lang: {
-        numericSymbols: ["K", "M", "B", "T", "P", "E"],
-        thousandsSep: ','
-    },
-    chart: {
-        style: {fontSize: '12pt', fontFamily: 'Sans-Serif'},
-        width: 400*1.6, height: 400 // golden ratio.
-    },
-    title: {text: '', align: 'left'}, 
-    subtitle: {align: 'left'}, 
-    legend: {verticalAlign: 'top', align: 'left', enabled: false, style: {fontSize: '14pt'}, padding: 0}, // we only show legend when grouping.
-    xAxis: {
-        startOnTick: true, endOnTick: true,
-        lineWidth: 0, tickWidth: 0, gridLineWidth: 1, 
-        title: {enabled: true, text: 'Values', style: {fontSize: '12pt'}}
-    },
-    yAxis: {
-        startOnTick: true, endOnTick: true,
-        labels: {enabled: false}, 
-        title: {enabled: false},
-    },
-    plotOptions: {
-        series: {
-           enableMouseTracking: false,
-           animation: false,
-           dataLabels: {
-              enabled: true,
-              backgroundColor: 'rgba(255, 255, 255, 0.5)',
-              padding: 0,
-              allowOverlap: false,
-              y: -3,
-              style: {
-                textOutline: 'none',
-                fontSize: '12pt'
-             }
-           }
-        },
-        line: {marker: { enabled: false }}
-     },
-    credits: {enabled: false}
-})});
+// document.addEventListener('DOMContentLoaded', function() { Highcharts.setOptions({
+//     lang: {
+//         numericSymbols: ["K", "M", "B", "T", "P", "E"],
+//         thousandsSep: ','
+//     },
+//     chart: {
+//         style: {fontSize: '12pt', fontFamily: 'Sans-Serif'},
+//         width: 400*1.6, height: 400 // golden ratio.
+//     },
+//     title: {text: '', align: 'left'}, 
+//     subtitle: {align: 'left'}, 
+//     legend: {verticalAlign: 'top', align: 'left', enabled: false, style: {fontSize: '14pt'}, padding: 0}, // we only show legend when grouping.
+//     xAxis: {
+//         startOnTick: true, endOnTick: true,
+//         lineWidth: 0, tickWidth: 0, gridLineWidth: 1, 
+//         title: {enabled: true, text: 'Values', style: {fontSize: '12pt'}}
+//     },
+//     yAxis: {
+//         startOnTick: true, endOnTick: true,
+//         labels: {enabled: false}, 
+//         title: {enabled: false},
+//     },
+//     plotOptions: {
+//         series: {
+//            enableMouseTracking: false,
+//            animation: false,
+//            dataLabels: {
+//               enabled: true,
+//               backgroundColor: 'rgba(255, 255, 255, 0.5)',
+//               padding: 0,
+//               allowOverlap: false,
+//               y: -3,
+//               style: {
+//                 textOutline: 'none',
+//                 fontSize: '12pt'
+//              }
+//            }
+//         },
+//         line: {marker: { enabled: false }}
+//      },
+//     credits: {enabled: false}
+// })});
 
